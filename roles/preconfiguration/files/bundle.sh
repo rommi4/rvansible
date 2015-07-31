26d23066478ba3f7e4f76410a03706712326f109
Roman Verenich <r_verenich@wargaming.net>
2015-07-31 19:18:43 +0300

    move to pre/post and main part of configuration

---
+++ roles/preconfiguration/files/bundle.sh
@@ -0,0 +1,28 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: bundle.sh
+# 
+#         USAGE: ./bundle.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 12/10/2014 14:20
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+rm -rf ~/.vim/bundle_available/*
+
+for i in $(ls ~/.vim/bundle);
+do
+  mv ~/.vim/bundle/$i ~/.vim/bundle_available 
+  ln -s ~/.vim/bundle_available/$i ~/.vim/bundle/$i
+done
